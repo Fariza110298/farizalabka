008980264be58d61445072d3c10a1a8b029e9e1d
Fariza Khalamkhan <khalamkhanf@gmail.com>
2018-05-02 10:48:51 +0600

    first commit

--- first_name/settings.py
+++ first_name/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = ')5dcsz#fnwm=#q!6)fg1*!e^0$^egv94sph!=t!z&y8i6bp(zx'
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+DEBUG = False
+#DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['127.0.0.1','localhost','fariza.herokuapp.com']
 
 
 # Application definition
